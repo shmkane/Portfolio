cec709f20a0a5aa404633d40f0442a5385da94f0
shmkane <shmkane@gmail.com>
2022-02-11 20:13:32 -0500

    Gatsby theme install

---
+++ gatsby-starter-portfolio-cara/src/@lekoarts/gatsby-theme-cara/components/footer.tsx
@@ -0,0 +1,30 @@
+/** @jsx jsx */
+import { Box, Flex, Link, jsx } from "theme-ui"
+
+const Footer = () => {
+
+  return (
+    <Box as="footer" variant="footer">
+      <Flex
+        sx={{
+          justifyContent: `center`,
+          alignItems: `center`,
+          mt: 3,
+          color: `text`,
+          fontWeight: `semibold`,
+          a: { color: `text` },
+        }}
+      >
+        Checkout my
+        <Link
+          aria-label="Link to my GitHub"
+          href="http://github.com/shmkane"
+        >
+          GitHub
+        </Link>
+      </Flex>
+    </Box>
+  )
+}
+
+export default Footer
